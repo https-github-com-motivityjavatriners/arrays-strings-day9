33957a5c437c031986ca7ee8ea991688bf007857
Venukumar9 <107048250+Venukumar9@users.noreply.github.com>
2022-07-26 14:26:49 +0530

    arrays and Strings updated

---
+++ Arrays/src/com/motivity/singleDimensionalArray/ArrayExample2.java
@@ -0,0 +1,15 @@
+package com.motivity.singleDimensionalArray;
+
+//Java Program to illustrate the use of declaration, instantiation  and initialization of Java array in a single line
+
+public class ArrayExample2 {
+    public static void main(String args[]){
+        int a[]={33,3,4,5};//declaration, instantiation and initialization
+             //printing array
+        for(int i=0;i<a.length;i++)     //length is the property of array
+            System.out.println(a[i]);
+
+    }
+
+
+}
