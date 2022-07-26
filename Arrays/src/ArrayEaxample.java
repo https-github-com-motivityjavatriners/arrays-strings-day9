68784123cc00baa65f6aeeb83d309ee42c41b302
Venukumar9 <107048250+Venukumar9@users.noreply.github.com>
2022-07-26 10:43:18 +0530

    Arrays and String -day 10 presentation

---
+++ Arrays/src/ArrayEaxample.java
@@ -0,0 +1,19 @@
+//Java Program to illustrate how to declare, instantiate, initialize
+
+public class ArrayEaxample{
+    public static void main(String[] args) {
+
+        int arr[]=new int[7];//declaration and instantiation
+        arr[0]=10;//initialization
+        arr[1]=20;
+        arr[2]=70;
+        arr[3]=40;
+        arr[4]=50;
+        arr[5]=60;
+        arr[6]=70;
+     //traversing array
+        for(int i=0;i<arr.length;i++)   //length is the property of array
+            System.out.println(arr[i]);
+    }
+
+}
